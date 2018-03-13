7b608728354e8b15492706a4615755d29b9614df
Givanaldo Rocha <givanaldo@gmail.com>
2018-03-13 09:28:35 -0300

    chat half-duplex

---
+++ Sockets/src/tcp/ChatClient.java
@@ -0,0 +1,39 @@
+package tcp;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ChatClient {
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+
+		int port = 9999;
+		String ipServidor = "localhost";
+
+		Socket cliente = new Socket(ipServidor, port);
+		System.out.println("O cliente se conectou ao servidor! Iniciando chat...");
+
+		Scanner teclado = new Scanner(System.in);
+		Scanner entrada = new Scanner(cliente.getInputStream());
+		PrintStream saida = new PrintStream(cliente.getOutputStream());
+		String mensagem;
+		System.out.print("cliente: ");
+		while (teclado.hasNextLine()) {			
+			mensagem = teclado.nextLine();
+			if (mensagem.equals("sair"))
+				break;
+			saida.println(mensagem);
+			mensagem = entrada.nextLine();
+			System.out.println("servidor: " + mensagem);
+			System.out.print("cliente: ");
+		}
+		entrada.close();
+		saida.close();
+		teclado.close();
+		cliente.close();
+		System.out.println("Conexão finalizada.");
+	}
+}
--- Sockets/src/tcp/EcoClient.java
+++ Sockets/src/tcp/EcoClient.java
@@ -11,22 +11,23 @@ public class EcoClient {
     public static void main(String[] args) throws UnknownHostException, IOException {
         
         int port = 9999;
-        String ipServidor = "127.0.0.1";
+        String ipServidor = "10.209.1.62";
         
         Socket cliente = new Socket(ipServidor, port);
         System.out.println("O cliente se conectou ao servidor!");
         
         Scanner teclado = new Scanner(System.in);
+        Scanner entrada = new Scanner(cliente.getInputStream());
         PrintStream saida = new PrintStream(cliente.getOutputStream());
         System.out.print("texto para enviar: ");
         while (teclado.hasNextLine()) {
             saida.println(teclado.nextLine());
-            Scanner entrada = new Scanner(cliente.getInputStream());
             System.out.print("Enviado pelo servidor: ");
             System.out.println(entrada.nextLine());
             System.out.print("texto para enviar: ");
         }
         saida.close();
+        entrada.close();
         teclado.close();
         cliente.close();
     }

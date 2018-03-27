0b8bc0ebb305b145c22d2a0759d33454d06ecddc
Givanaldo Rocha <givanaldo@gmail.com>
2018-03-27 15:57:16 -0300

    Atualizacao

--- Sockets/src/chat/ClientChat.java
+++ Sockets/src/chat/ClientChat.java
@@ -9,13 +9,11 @@ import java.util.Scanner;
 public class ClientChat {
 
     public static void main(String[] args) throws UnknownHostException, IOException {
-
         // dispara cliente
         Scanner teclado = new Scanner(System.in);
         System.out.println("Usuário: ");
         String nomeCliente = teclado.nextLine();
         new ClientChat("127.0.0.1", 12345, nomeCliente).executa();
-
     }
 
     private String host;
@@ -33,8 +31,8 @@ public class ClientChat {
         System.out.println("O cliente se conectou ao servidor!");
 
         // thread para receber mensagens do servidor
-        ReceptorChat r = new ReceptorChat(cliente.getInputStream());
-        new Thread(r).start();
+        ReceptorChat receptor = new ReceptorChat(cliente.getInputStream());
+        new Thread(receptor).start();
 
         // lê msgs do teclado e manda pro servidor
         Scanner teclado = new Scanner(System.in);
@@ -43,10 +41,9 @@ public class ClientChat {
         while (teclado.hasNextLine()) {
             saida.println(nome + ": " + teclado.nextLine());
         }
-
+        
         saida.close();
         teclado.close();
         cliente.close();
-
     }
 }
